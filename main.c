efe2c2e23981d5c2614c937fe05557cfbe1c4023
Alric Le Conte <ale-cont@student.42lyon.fr>
2022-12-20 14:28:49 +0100

    all done, ready to be pushed

--- main.c
+++ main.c
@@ -6,29 +6,52 @@
 /*   By: ale-cont <ale-cont@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/14 11:45:33 by ale-cont          #+#    #+#             */
-/*   Updated: 2022/12/16 15:15:29 by ale-cont         ###   ########.fr       */
+/*   Updated: 2022/12/20 14:27:32 by ale-cont         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-static int check_format(char **argv)
+static int	check_mul_arg(char **argv)
 {
 	int	i;
 	int	j;
 	int	len;
 
 	i = 0;
+	j = -1;
+	len = 0;
 	while (argv[++i])
 	{
 		j = -1;
 		len = 0;
 		while (argv[i][++j])
+		{
 			if (argv[i][j] >= '0' && argv[i][j] <= '9')
-				len++;
+					len++;
+			if (argv[i][j] == ' ')
+				return (0);
+		}
 		if (len == 0)
 			return (0);
 	}
+	if (len == 0)
+		return (0);
+	return (1);
+}
+
+static int	check_single(char **argv)
+{
+	int	j;
+	int	len;
+
+	j = -1;
+	len = 0;
+	while (argv[1][++j])
+		if (argv[1][j] >= '0' && argv[1][j] <= '9')
+			len++;
+	if (len == 0)
+		return (0);
 	return (1);
 }
 
@@ -54,7 +77,9 @@ int	main(int argc, char *argv[])
 
 	if (argc <= 1 || (argv[1][0] == '\0' && argc == 2))
 		return (0);
-	if (check_format(argv) == 0)
+	if (argc == 2 && check_single(argv) == 0)
+		return (ft_putstr_fd("Error", STDERR_FILENO), 0);
+	else if (argc != 2 && check_mul_arg(argv) == 0)
 		return (ft_putstr_fd("Error", STDERR_FILENO), 0);
 	size = parse_args(argc, &argv);
 	if (size == -1)
